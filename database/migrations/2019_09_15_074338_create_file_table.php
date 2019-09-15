4e651a40d3dc624f92fcc007970363fbc94eaae1
VanHoa <vanhoapt01@gmail.com>
2019-09-15 14:53:20 +0700

    some message

---
+++ database/migrations/2019_09_15_074338_create_file_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFileTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('file', function (Blueprint $table) {
+				$table->increments('id');
+				$table->string('name');
+				$table->string('path');
+				$table->integer('kind_id')->unsigned();
+				$table->foreign('kind_id')->references('id')->on('kind')->onDelete('cascade');
+			});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('file');
+	}
+}
